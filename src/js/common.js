b51d800dca076c33a95a93a21886e21c6c526e37
UniiX <alexey@developus.ru>
2022-08-04 20:27:23 +0300

    Подправил 3 блока (Страны, Заболевания, Процедуры)

--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,5 @@
-const splide = new Splide( '.splide' ).mount();
+var splides = document.getElementsByClassName( 'splide' );
+
+for ( var i = 0; i < splides.length; i++ ) {
+  new Splide( splides[ i ] ).mount();
+}
